62f96f136f2467a5afbcf9df4d12094af24c4806
Kevin Deng <31214837+kevindjf66@users.noreply.github.com>
2017-09-30 11:28:08 -0400

    Create Prac9.py

---
+++ Prac9.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 10:36:12 2017
+
+@author: Kevin Deng
+"""
+
+#prime factorization for a number
+
+n = int(raw_input('Please enter your number:'))
+print '{} = '.format(n),
+if n == 1:
+    print '1 = 1'
+else:
+    while n != 1:
+        for i in range(2, n+1):
+            if n == 1:
+                break
+            if n % i == 0:
+                print '{} * '.format(i),
+                n = n / i
+print '{}'.format(1),
